eee6fbf9c5483ba1e53a908dc945b822c4fd9edb
Administrator <Administrator@panjie>
2012-09-12 16:01:44 +0800

    物资管理的物资列表，入库登记的添加物资有问题。

--- Lib/Action/GoodAction.class.php
+++ Lib/Action/GoodAction.class.php
@@ -3,13 +3,22 @@
 class GoodAction extends BaseAction {
 
     public static $method_array = array(
-        "index" => "物资管理首页",
+        
         "checkin" => "入库登记",
         "checkout" => "出库登记",
-        "endgood" => "领用登记"
+        "endgood" => "领用登记",
+        "index" => "物资管理"
     );
 
     public function index() {
+        $m_good = M("Good");
+        import("ORG.Util.Page");
+        $count = $m_good->count();
+        $p = new Page($count, self::RECORDS_ONE_PAGE);
+        $page = $p->show();
+        $goodList = $m_good->order("id desc")->limit($p->firstRow . ',' . $p->listRows)->select();
+        $this->assign("goods", $goodList);
+        $this->assign("page", $page);
         $this->display();
     }
 
