8c575684a0fe48cf026c9db0cb175c90b54c5613
Administrator <Administrator@panjie>
2012-12-28 16:26:27 +0800

    修改

--- Lib/Model/GoodModel.class.php
+++ Lib/Model/GoodModel.class.php
@@ -5,7 +5,7 @@ class GoodModel extends RelationModel {
     protected $_validate = array(
         array("serial", "require", "物资编号必须", Model::EXISTS_VALIDATE, "", Model:: MODEL_BOTH),
         array("name", "require", "物资名必须", Model::EXISTS_VALIDATE, "", Model:: MODEL_BOTH),
-        array("donater_id", "require", "捐赠人必须", Model::EXISTS_VALIDATE, "", Model:: MODEL_BOTH),
+        //array("donater_id", "require", "捐赠人必须", Model::EXISTS_VALIDATE, "", Model:: MODEL_BOTH),
         array("serial", "", "编号已存在", Model::EXISTS_VALIDATE, "unique", Model:: MODEL_BOTH),
     );
     protected $_auto = array(
