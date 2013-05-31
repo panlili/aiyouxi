ecbc449f0a52d5624aa74e94a30c4e2018f97d9a
Administrator <Administrator@2013-0104-0830>
2013-05-31 10:49:09 +0800

    修改数据库的sql，在原有数据库上运行这个sql完成数据库升级

---
+++ Resource/Database/add20130531-1.sql
@@ -0,0 +1,17 @@
+﻿ALTER TABLE `ayx_donater`
+ADD COLUMN `location`  int(11) NULL COMMENT '地点' AFTER `uptime`;
+ALTER TABLE `ayx_family`
+ADD COLUMN `location`  int(11) NULL COMMENT '地点' AFTER `uptime`;
+ALTER TABLE `ayx_good`
+ADD COLUMN `location`  int(11) NULL COMMENT '地点' AFTER `uptime`;
+ALTER TABLE `ayx_record`
+ADD COLUMN `location`  int(11) NULL COMMENT '地点' AFTER `uptime`;
+ALTER TABLE `ayx_user`
+ADD COLUMN `location`  int(11) NULL COMMENT '地点' AFTER `lastloginip`;
+CREATE TABLE `ayx_location` (
+`id`  int(11) UNSIGNED NOT NULL AUTO_INCREMENT ,
+`name`  varchar(50)  NULL COMMENT '地点名' ,
+`comment`  text  NULL COMMENT '有关地点的注释' ,
+PRIMARY KEY (`id`)
+)
+;
